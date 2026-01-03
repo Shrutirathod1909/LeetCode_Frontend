d72a891967d3bbe7c1ee83e6a4391e5d968b7be4
Shrutirathod1909 <shrutirathod1909@gmail.com>
2026-01-03 18:44:38 -0500

    Add auth token to axios client

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -8,4 +8,15 @@ const axiosClient = axios.create({
   },
 });
 
+// ✅ ADD THIS PART
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default axiosClient;
