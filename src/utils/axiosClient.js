7bde0aefcf10ebaa8344bfebc958022be19780af
Shrutirathod1909 <shrutirathod1909@gmail.com>
2026-01-03 20:38:44 -0500

    Ready for Netlify deploy

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -2,14 +2,17 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-  headers: { "Content-Type": "application/json" },
+  withCredentials: true,  // cookie send/receive
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Attach token from localStorage
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
