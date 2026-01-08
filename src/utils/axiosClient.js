95363d4e8b98672860f69f63faa84cd69525b19e
Shrutirathod1909 <shrutirathod1909@gmail.com>
2026-01-08 12:46:45 -0500

    Initial commit

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,23 +1,11 @@
-// src/utils/axiosClient.js
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // ✅ FIX HERE
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, // 🔥 IMPORTANT for cookies
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-axiosClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default axiosClient;
